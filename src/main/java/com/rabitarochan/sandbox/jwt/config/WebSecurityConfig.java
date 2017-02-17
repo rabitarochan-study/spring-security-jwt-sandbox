d2c354ad15c147913022d6faba825298d5cf8a44
rabitarochan <rabitarochan@gmail.com>
2017-02-18 01:51:17 +0900

    Add minimal Spring Security config code

---
+++ src/main/java/com/rabitarochan/sandbox/jwt/config/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.rabitarochan.sandbox.jwt.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Spring Security settings.
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
