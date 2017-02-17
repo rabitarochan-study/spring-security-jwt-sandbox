bbac72824f57d27801c5b3c1de7930fddc8ed170
rabitarochan <rabitarochan@gmail.com>
2017-02-18 02:18:38 +0900

    Create not redirect AuthenticationEntryPoint

--- src/main/java/com/rabitarochan/sandbox/jwt/config/WebSecurityConfig.java
+++ src/main/java/com/rabitarochan/sandbox/jwt/config/WebSecurityConfig.java
@@ -1,8 +1,13 @@
 package com.rabitarochan.sandbox.jwt.config;
 
+import com.rabitarochan.sandbox.jwt.security.JwtAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
 
 /**
  * Spring Security settings.
@@ -16,4 +21,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("admin").password("secret").roles("ADMIN");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+
+        // Don't redirect to login page when not authenticated.
+        http.exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint());
+    }
+
+    @Bean
+    public AuthenticationEntryPoint jwtAuthenticationEntryPoint() {
+        return new JwtAuthenticationEntryPoint();
+    }
 }
